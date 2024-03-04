26904d03a5d105479f1afb3e7488d38153f97759
alokprasad <aptheengineer@gmail.com>
2024-03-04 20:31:16 +0530

    numpyramid

---
+++ src/NumericPattern/NumPyramid.java
@@ -0,0 +1,13 @@
+package NumericPattern;
+
+public class NumPyramid {
+    public static void main(String args[]){
+        int num=1;
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
